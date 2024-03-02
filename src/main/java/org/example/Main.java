b675c0e0767a84b86835f2642ac4e140477c34e8
Veronica <verosehe@gmail.com>
2024-03-02 12:37:05 +0100

    Versión 3.1, introducción del método initAgenda.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         // Crear una nueva agenda.
-        Agenda agendaContactos = new Agenda();
-
-        // Añadir contactos a la agenda.
-        agendaContactos.addContact("John Doe", "1234567890");
-        agendaContactos.addContact("Jane Doe", "9876543210");
-        agendaContactos.addContact("Mary Jane", "1122334455");
+        Agenda agendaContactos = initAgenda();
 
         // Imprimir todos los contactos de la agenda.
         System.out.println("Todos los contactos: ");
@@ -31,5 +26,15 @@ public class Main {
         for (Contacto c : agendaContactos.getContacts()) {
             System.out.println(c.getName() + " -> " + c.getPhones());
         }
+    }
+
+    private static Agenda initAgenda() {
+        Agenda agendaContactos = new Agenda();
+
+        // Añadir contactos a la agenda.
+        agendaContactos.addContact("John Doe", "1234567890");
+        agendaContactos.addContact("Jane Doe", "9876543210");
+        agendaContactos.addContact("Mary Jane", "1122334455");
+        return agendaContactos;
     }
 }
